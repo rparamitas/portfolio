90ebf45727130bfe489f2adbe8e4e4cc56aeadd2
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2025-02-28 06:13:44 +0700

    add style on navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,9 @@ import { listPage } from "@/config/site";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { Logo } from "./footer";
+import { ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { josefinSans } from "@/app/font";
 
 const Navbar = () => {
   const route = useRouter();
@@ -14,7 +17,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="sticky top-0 z-10 flex items-center justify-center bg-white">
       <nav className="container flex h-12 items-center justify-between">
         <Logo />
         <ul className="flex flex-row space-x-4 text-lg">
@@ -22,8 +25,16 @@ const Navbar = () => {
             <li
               key={item.title}
               onClick={() => route.push(item.url)}
-              className="cursor-pointer"
+              className={cn(
+                "group flex transform cursor-pointer flex-row items-center justify-center text-xl transition-transform hover:translate-x-2",
+                josefinSans.className,
+                listPage.find((page) => page.url === pathname)?.title ===
+                  item.title && "underline",
+              )}
             >
+              <ChevronRight
+                className={cn("invisible h-4 w-4 group-hover:visible")}
+              />
               {item.title}
             </li>
           ))}
